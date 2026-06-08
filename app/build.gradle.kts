e0a771933b3d69ed6498d9014dab2350b85a7daf
DT-Mac <dtkelvin9@gmail.com>
2026-06-08 21:55:46 +0800

    Release DrivePad 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.drivepad.app"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
